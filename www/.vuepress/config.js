26746e3cde350a93b0a5bb7d50137eedde7c8e6b
Andrés Tuells <zentropia@gmail.com>
2019-09-12 19:46:48 +0200

    deploy contents

---
+++ www/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: 'The Craziest Project',
+  description: 'The only way to be sane is to be mad.',
+  lastUpdated: 'Last Updated', // string | boolean
+  markdown: {
+    config: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  },
+  themeConfig: {
+    displayAllHeaders: true, // Default: false
+    sidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'Schrânk!', link: '/schrank'
+      },
+      {
+        text: 'Guruguru', link: '/guruguru'
+      },
+      {
+        text: 'Contact', link: '/contact'
+      },
+    ]
+    
+  }
+}
